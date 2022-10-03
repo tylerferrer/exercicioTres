9477dfbb7dd4ca14ed8bb43a76de2d6eae7970f2
tylerferrer <tylerlucasferrer@gmail.com>
2022-10-03 19:40:19 -0300

    adição de comentarios

--- src/main/java/com/mycompany/exercicio3/Exercicio3.java
+++ src/main/java/com/mycompany/exercicio3/Exercicio3.java
@@ -10,8 +10,6 @@ public class Exercicio3 {
 
     public static void main(String[] args) {
         double numero1, numero2, resultado;
-//        double numero2;
-//        double resultado;
         Scanner entrada = new Scanner(System.in);
         
         System.out.println("Digite os números que deseja somar: ");
@@ -21,8 +19,7 @@ public class Exercicio3 {
         
         resultado = numero1 + numero2;
         
-        System.out.print("O resultado da soma é: ");
-        System.out.print(resultado);
+        System.out.print("O resultado da soma é: " + resultado);
         
         
     }
