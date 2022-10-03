5ac4d680c0f4fc6867f9bfcb255b26fff68b322a
tylerferrer <tylerlucasferrer@gmail.com>
2022-10-03 19:42:04 -0300

    adição de comentarios

--- src/main/java/com/mycompany/exercicio3/Exercicio3.java
+++ src/main/java/com/mycompany/exercicio3/Exercicio3.java
@@ -9,14 +9,17 @@ import java.util.Scanner;
 public class Exercicio3 {
 
     public static void main(String[] args) {
+//        Variaveis e scanner
         double numero1, numero2, resultado;
         Scanner entrada = new Scanner(System.in);
         
+//        Entrada de dados pelo usuário
         System.out.println("Digite os números que deseja somar: ");
         numero1 = entrada.nextDouble();
         numero2 = entrada.nextDouble();
         entrada.close();
         
+//        Operação feita para a soma das entradas
         resultado = numero1 + numero2;
         
         System.out.print("O resultado da soma é: " + resultado);
